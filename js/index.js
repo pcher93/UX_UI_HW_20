6b28e28691540a2ab9ee0ac9fde930a3927df953
pcher93 <109242086+pcher93@users.noreply.github.com>
2023-03-05 21:32:56 -0600

    add boostrap dropdown menu

--- js/index.js
+++ js/index.js
@@ -10,3 +10,15 @@ $("nav a").on({
       window.location.href = "https://drive.google.com/file/d/1ZD_du3jUAtHHFFzkJ_Xif80cz67BFg6n/view";
     });
   });
+
+  var dropdowns = document.querySelectorAll('.dropdown');
+
+  dropdowns.forEach(function(dropdown) {
+    dropdown.addEventListener('mouseenter', function() {
+      dropdown.querySelector('.dropdown-menu').style.display = 'block';
+    });
+  
+    dropdown.addEventListener('mouseleave', function() {
+      dropdown.querySelector('.dropdown-menu').style.display = 'none';
+    });
+  });
